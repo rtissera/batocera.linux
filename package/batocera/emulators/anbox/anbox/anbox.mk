aa379f6c8778d998de4ab8a88ab2189a9431739f
Romain TISSERAND <romain.tisserand@gmail.com>
2020-08-30 10:00:13 +0200

    Bump anbox, try to fix sdbus-cpp and libsystemd not compiling

--- package/batocera/emulators/anbox/anbox/anbox.mk
+++ package/batocera/emulators/anbox/anbox/anbox.mk
@@ -3,13 +3,13 @@
 # ANBOX
 #
 ################################################################################
-# Version.: Commits on Jul 29, 2020
-ANBOX_VERSION = 4d84370b73852e5ca755a28d0304c4d7d0aa589c
+# Version.: Commits on Aug 21, 2020
+ANBOX_VERSION = c1e75506bad0c479c8591970ce422aa8646654ac
 ANBOX_SITE = https://github.com/anbox/anbox.git
 ANBOX_SITE_METHOD=git
 ANBOX_GIT_SUBMODULES=YES
 ANBOX_LICENSE = GPLv2
-ANBOX_DEPENDENCIES = boost protobuf sdl2_image lxc properties-cpp glm
+ANBOX_DEPENDENCIES = boost protobuf sdl2_image lxc properties-cpp glm libcap expat host-expat
 
 # Should be set when the package cannot be built inside the source tree but needs a separate build directory.
 ANBOX_SUPPORTS_IN_SOURCE_BUILD = NO
