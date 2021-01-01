1e98b18bf76dacb59de49f6e2214ae1c0e01a095
Romain TISSERAND <romain.tisserand@gmail.com>
2021-01-02 00:07:31 +0100

    WIP future melonDS nogui stuff

--- package/batocera/emulators/melonds/melonds.mk
+++ package/batocera/emulators/melonds/melonds.mk
@@ -9,12 +9,19 @@ MELONDS_SITE = https://github.com/Arisotura/melonDS.git
 MELONDS_SITE_METHOD=git
 MELONDS_GIT_SUBMODULES=YES
 MELONDS_LICENSE = GPLv2
-MELONDS_DEPENDENCIES = sdl2 qt5base slirp
+MELONDS_DEPENDENCIES = sdl2 slirp
 
 # Should be set when the package cannot be built inside the source tree but needs a separate build directory.
 MELONDS_SUPPORTS_IN_SOURCE_BUILD = NO
 
 MELONDS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
+MELONDS_CONF_OPTS += -DBUILD_QT_SDL=ON
+DUCKSTATION_DEPENDENCIES += qt5base
+else
+MELONDS_CONF_OPTS += -DBUILD_QT_SDL=OFF
+endif
 
 MELONDS_CONF_ENV += LDFLAGS=-lpthread
 
