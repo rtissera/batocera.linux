5cc553d0d5a9ba324daa450de60e603539c5633b
Romain TISSERAND <romain.tisserand@gmail.com>
2021-02-02 22:43:40 +0100

    Bump SDLPoP

--- package/batocera/emulators/sdlpop/sdlpop.mk
+++ package/batocera/emulators/sdlpop/sdlpop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Jun 24, 2020
-SDLPOP_VERSION = a7dbbe15c7d3291c80be09c2d4542e6e63681d3c
+SDLPOP_VERSION = 7b0e4027d68e910cad2aaa70e18701aad0e011e4
 SDLPOP_SITE = $(call github,NagyD,SDLPoP,$(SDLPOP_VERSION))
 SDLPOP_SUBDIR = src
 SDLPOP_LICENSE = GPLv3
