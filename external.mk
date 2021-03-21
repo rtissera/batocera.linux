65d4a275546b74255bf82367af161e69c80fe1fc
Romain TISSERAND <romain.tisserand@gmail.com>
2021-03-22 00:04:56 +0100

    WIP compiler-commons implementation. Breaks EasyRPG.

--- external.mk
+++ external.mk
@@ -1,1 +1,2 @@
+include $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils-host/compiler-commons/compiler-commons.mk
 include $(sort $(wildcard $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/*/*.mk $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/*/*/*.mk $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/*/*/*/*.mk $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/*/*/*/*/*.mk))
